c1e14622438f78e00db55ea76ec069e22f3e1772
Rafael Forte <rcforte@Rafaels-MacBook.home>
2018-12-29 13:32:31 -0500

    Adding comments to the code

--- portfolio_analytics.py
+++ portfolio_analytics.py
@@ -19,40 +19,88 @@ pd.set_option('display.width', 82)
 pd.set_option('precision', 3)
 
 def get_historical_closes(ticker, start_date, end_date):
-    f = True
+    first_time = True
+
+    # For each ticker
     for t in ticker:
+        # get the data from yahoo finance
         pnl = web.DataReader(t, 'yahoo', start_date, end_date)
+
+        # add a column for the ticker and call it minor
         pnl['minor'] = t
+
+        # extract a new dataframe only with
+        # the minor and adj close columns
         tmp = pnl[['minor', 'Adj Close']].reset_index()
-        tmp.rename(columns={'minor':'Ticker', 'Adj Close':'Close'}, inplace=True)
-        if f:
+
+        # rename the columns to tiker and close
+        tmp.rename(
+            columns={'minor':'Ticker', 'Adj Close':'Close'},
+            inplace=True
+        )
+
+        if first_time:
+            # if first ticker, just assign df
             df = tmp
-            f = False
+            first_time = False
         else:
+            # if has tickers already, concat
+            # to the dataframe
             df = pd.concat([df,tmp])
+
+    # pivot the dataframe by ticker
     piv = df.pivot(index='Date', columns='Ticker')
+
+    # pivoting creates a multi-index with
+    # 2 levels since. We need to make sure
+    # we remove the first level, which is
+    # close price
     piv.columns = piv.columns.droplevel(0)
     return piv
 
 def calc_daily_returns(closes):
+    # calculates the returns, and converts it
+    # to log space so that it is easier to
+    # annualize
     return np.log(closes/closes.shift(1))
 
 def calc_annual_returns(daily_returns):
+    # takes all daily returns of a given year
+    # sums them up and converts back to arithmetic
+    # space. Notice we can add daily returns because
+    # they are in log space
     return np.exp(daily_returns.groupby(lambda date: date.year).sum())-1
 
 def calc_portfolio_var(returns, weights=None):
     n = returns.columns.size
     if weights is None:
+        # equally distributes the weights
         weights = np.ones(n)/n
+
+    # Takes the cov matrix of the returns
+    # ddof returns a simple average
     sigma = np.cov(returns.T, ddof=0)
+
+    # applies the portfolio weights to the
+    # cov matrix so that it becomes the portfolio
+    # cov matrix instead of stock returns cov matrix
     return (weights * sigma * weights.T).sum()
 
 def sharpe_ratio(returns, weights=None, risk_free_rate=0.015):
     n = returns.columns.size
     if weights is None:
+        # equally distributes the weights
         weights = np.ones(n)/n
+
+    # calculate the cov matrix
     var = calc_portfolio_var(returns, weights)
+
+    # calculate the returns mean
     means = returns.mean()
+
+    # calculate the portfolio mean returns
+    # subtracts from the risk free rate and
+    # divides by the portfolio std
     return (means.dot(weights) - risk_free_rate)/np.sqrt(var)
 
 def negative_sharpe_ratio_n_minus_1_stock(weights, returns, risk_free_rate):
@@ -95,7 +143,7 @@ def calc_efficient_frontier(returns):
                 method='SLSQP',
                 constraints=constraints,
                 bounds=bounds
-        )
+                )
         if not results.success:
             raise Exception(result.message)
         result_means.append(np.round(r,4))
@@ -123,12 +171,9 @@ if __name__ == '__main__':
     daily_returns = calc_daily_returns(closes)
     annual_returns = calc_annual_returns(daily_returns)
     portfolio_var = calc_portfolio_var(annual_returns)
+    print(np.sqrt(portfolio_var))
     sr = sharpe_ratio(annual_returns)
     optimized_result = optimize_portfolio(annual_returns, 0.0003)
-    print(optimized_result)
-
     frontier_data = calc_efficient_frontier(annual_returns)
-    print(frontier_data)
-
     plot_efficient_frontier(frontier_data)
 
